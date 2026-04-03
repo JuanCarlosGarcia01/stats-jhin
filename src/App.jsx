22fb0ac913aa8d801dff6aee632adf0d2f24dbbb
JuanCarlos <molachineitor123@gmail.com>
2026-04-02 21:51:12 -0300

    Final posta

--- src/App.jsx
+++ src/App.jsx
@@ -127,8 +127,8 @@ export default function App() {
     >
       <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
         <h1 style={{ fontSize: "48px", marginBottom: "10px" }}>
-          Jhin Winrate Tracker 🔥 UPDATED
-        </h1>
+          🔥Jhin Winrate Tracker🔥 
+        </h1><br /><br />
 
         <div
           style={{
